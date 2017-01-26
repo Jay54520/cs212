2d11c48e91f55cd26737c2628d95e72a9aea2580
ja54520 <simin_jie@163.com>
2017-01-26 15:56:05 +0800

    test:  unit 4:  `bridge_problem`: make it more efficient by reducing `duplicate` state

--- unit4/bridge_problem.py
+++ unit4/bridge_problem.py
@@ -4,6 +4,7 @@ __author__ = 'Simon'
 
 Fail = []
 
+
 def elapsed_time(path):
     """
     return final elapsed_time of this path
@@ -43,10 +44,10 @@ def path_cost(path):
     if len(path) < 3:
         return 0
     else:
-        return sum([bcost(action) for action in path_actions(path)])
+        return path[-2][-1]
 
 
-def bsuccessors(state):
+def bsuccessors(state, t):
     """Return a dict of {state:action} pairs. A `state` is a (here, there, t) tuple,
     where `here` and `there` are frozensets of people (indicated by their times) and/or
     the `light`, and `t` is a number indicating the elapsed time. Action is represented
@@ -55,7 +56,7 @@ def bsuccessors(state):
 
     select two people(if people is same, then indicate one person) from here or there besides `light`
     """
-    here, there, t = state
+    here, there = state
     if not here:
         return here
     successors = {}
@@ -66,16 +67,48 @@ def bsuccessors(state):
                 for b in here:
                     if b is not 'light':
                         tmp_fronzenset = frozenset([a, b, 'light'])
-                        successors[(here - tmp_fronzenset, there | tmp_fronzenset, t + max(a, b) )] = ['%s, %s, light ->' % (a, b)]
+                        successors[(here - tmp_fronzenset, there | tmp_fronzenset)] = ['%s, %s, light ->' % (a, b), t + max(a, b)]
     else:
         for a in there:
             if a is not 'light':
                 for b in there:
                     if b is not 'light':
                         tmp_fronzenset = frozenset([a, b, 'light'])
-                        successors[(here | tmp_fronzenset, there - tmp_fronzenset, t + max(a, b))] = ['%s, %s, light <-' % (a, b)]
+                        successors[(here | tmp_fronzenset, there - tmp_fronzenset)] = ['%s, %s, light <-' % (a, b), t + max(a, b)]
 
     return successors
+
+
+def final_state(path):
+    return path[-1]
+
+
+def add_path_to_frontier(frontier, path):
+    """Add path to frontier, replacing costlier path if there is one"""
+    old = None
+    # two different paths end in the same state, so we want to remove the
+    # costlier path
+    for i, p in enumerate(frontier):
+        if final_state(p) == final_state(path):
+            old = i
+            break
+    if old is not None:
+        if path_cost(frontier[old]) > path_cost(path):
+            del frontier[old]  # old path is worse, so delete it
+            # -------------------add current path, and resort-------------------
+            frontier.append(path)
+            frontier.sort(key=path_cost)
+            return frontier
+            # -------------------add current path, and resort-------------------
+        else:
+            # old path is better, so don't append current path, just return
+            return frontier
+    else:
+        # -------------------add current path, and resort-------------------
+        frontier.append(path)
+        frontier.sort(key=path_cost)
+        return frontier
+        # -------------------add current path, and resort-------------------
 
 
 def bridge_problem(here):
@@ -86,29 +119,25 @@ def bridge_problem(here):
     """
     # initialize, the `light` is in `here`
     here = frozenset(here) | frozenset(['light'])
-    frontier = [ [(here, frozenset(), 0)], ]
+    frontier = [ [(here, frozenset())], ]
     explored = set()
-
-    if not here:
-        return frontier[0]
 
     while frontier:
         path = frontier.pop(0)
-        here, _, _ = state1 = path[-1]
-        # test the result later because we will sort after path being
-        # appended
-        if not here:
+        t = path_cost(path)
+        here, there, = state1 = final_state(path)
+        if not here or (len(here) == 1 and 'light' in here):
             return path
-        for state, action in bsuccessors(path[-1]).items():
+        # TODO why this code move to `for` loop below will take more minutes?
+        explored.add(state1)
+        for state, action in bsuccessors(state1, t).items():
             if state not in explored:
-                explored.add(state)
+                # explored.add(state)
                 path2 = path + [action, state]
-                here, _, _ = state
-                frontier.append(path2)
-                # sort by elapsed_time, then will return shortest path first
-                frontier.sort(key=elapsed_time)
+                add_path_to_frontier(frontier, path2)
 
 
 result = bridge_problem([1, 2, 5, 10])
+print(result[-2])
 print(result)
-print(bsuccessors( ( frozenset([1, 2, 3, 'light']), frozenset(), 0 ) ))
+# print(bsuccessors( ( frozenset([1, 2, 3, 'light'],), frozenset()), 0))
